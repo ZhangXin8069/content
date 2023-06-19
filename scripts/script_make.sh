748a34d265e46f41518a9380626e97606263521f
zhangxin <zhangxin8069@qq.com>
2023-06-19 13:32:36 +0800

    2023-06-19-13-32-36

--- scripts/script_make.sh
+++ scripts/script_make.sh
@@ -1,11 +1,14 @@
 # source
-_PATH=`pushd "$(dirname "$0")";pwd`
+_PATH=$(
+       pushd "$(dirname "$0")"
+       pwd
+)
 pushd ${_PATH}/../
 source ./env.sh
 popd
 
 # init
-_NAME=`basename "$0"`
+_NAME=$(basename "$0")
 name='work'
 work_name="test"
 tmp_name="tmp"
@@ -16,10 +19,10 @@ tmp_path=${_HOME}/${tmp_name}
 pushd ${tmp_path}
 echo "###${_NAME} is running...:$(date "+%Y-%m-%d-%H-%M-%S")###"
 for ((i = 0; i < 10; i++)); do
-    echo "making ${name}${i}.sh in ${tmp_path}"
-    echo "pushd ${tmp_path}" >${name}${i}.sh
-    echo "nvcc -o ${name}${i} ${work_path}/${name}${i}.cu && ./${name}${i}" >>${name}${i}.sh
-    echo "popd" >>${name}${i}.sh
+       echo "making ${name}${i}.sh in ${tmp_path}"
+       echo "pushd ${tmp_path}" >${name}${i}.sh
+       echo "nvcc -o ${name}${i} ${work_path}/${name}${i}.cu && ./${name}${i}" >>${name}${i}.sh
+       echo "popd" >>${name}${i}.sh
 done
 echo "###${_NAME} is done......:$(date "+%Y-%m-%d-%H-%M-%S")###"
 popd
@@ -27,27 +30,24 @@ popd
 pushd ${tmp_path}
 echo "###${_NAME} is running...:$(date "+%Y-%m-%d-%H-%M-%S")###"
 name="zwork"
-for i in $(seq 16)
-do
-echo "making ${name}${i}.sh in ${tmp_path}"
-       echo "pushd ${work_path}" > ${name}${i}.sh
-       echo "mpic++ ${name}.cc -o ${name}${i} && mpirun -np ${i} ${name}${i}" >> ${name}${i}.sh
+for i in $(seq 16); do
+       echo "making ${name}${i}.sh in ${tmp_path}"
+       echo "pushd ${work_path}" >${name}${i}.sh
+       echo "mpic++.openmpi ${name}.cc -o ${name}${i} && mpirun.openmpi -np ${i} ${name}${i}" >>${name}${i}.sh
        echo "popd" >>${name}${i}.sh
 done
 name="zexample"
-for i in $(seq 16)
-do
-echo "making ${name}${i}.sh in ${tmp_path}"
-       echo "pushd ${work_path}" > ${name}${i}.sh
-       echo "mpic++ ${name}.cc -o ${name}${i} && mpirun -np ${i} ${name}${i}" >> ${name}${i}.sh
+for i in $(seq 16); do
+       echo "making ${name}${i}.sh in ${tmp_path}"
+       echo "pushd ${work_path}" >${name}${i}.sh
+       echo "mpic++.openmpi ${name}.cc -o ${name}${i} && mpirun.openmpi -np ${i} ${name}${i}" >>${name}${i}.sh
        echo "popd" >>${name}${i}.sh
 done
 name="ztest"
-for i in $(seq 16)
-do
-echo "making ${name}${i}.sh in ${tmp_path}"
-       echo "pushd ${work_path}" > ${name}${i}.sh
-       echo "mpic++ ${name}.cc -o ${name}${i} && mpirun -np ${i} ${name}${i}" >> ${name}${i}.sh
+for i in $(seq 16); do
+       echo "making ${name}${i}.sh in ${tmp_path}"
+       echo "pushd ${work_path}" >${name}${i}.sh
+       echo "mpic++.openmpi ${name}.cc -o ${name}${i} && mpirun.openmpi -np ${i} ${name}${i}" >>${name}${i}.sh
        echo "popd" >>${name}${i}.sh
 done
 echo "###${_NAME} is done......:$(date "+%Y-%m-%d-%H-%M-%S")###"
